22c0727d807b8c0a630c6ba35b0b6b8c8fe084d6
joker153 <77005994+joker153@users.noreply.github.com>
2021-05-19 11:44:41 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,21 +5,21 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<b>ഈ ബോട്ട് തികച്ചും @MGmoviegram ഗ്രൂപ്പിലേക്ക് ആണ്. ഞാൻ ആണ് ആ ഗ്രൂപ്പിൽ സിനിമകൾ നൽകുന്നത്😁</b>
+<b>ഈ ബോട്ട് തികച്ചും @movieuploaders ഗ്രൂപ്പിലേക്ക് ആണ്. ഞാൻ ആണ് ആ ഗ്രൂപ്പിൽ സിനിമകൾ നൽകുന്നത്😁</b>
 """    
     
     HELP_TEXT = """
-My CHANNEL @MG_MEDIA 
-MY GROUP     @MGMOVIEGRAM
+My CHANNEL https://t.me/joinchat/Rj8lxYv8Cro1ODc1 
+MY GROUP     @movieuploaders
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/FayasNoushad">Fayas Noushad</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Rfxjoker">Rfxjoker</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/Rfxjoker">Click Me</a></i>
 """
